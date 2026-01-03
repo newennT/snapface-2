07a7d0febc1d86032008c724e3704db6d4ce545f
NewennT <newenn.turbiau@gmail.com>
2026-01-03 18:34:38 +0100

    setup + Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Observable } from 'rxjs';
+import { map, filter, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  ngOnInit() {}
+  interval$!: Observable<string>;
+  ngOnInit() {
+    this.interval$ = interval(1000).pipe(
+      filter(value => value % 3 === 0),
+      map(value => value % 2 === 0 ? `${value} is EVEN` : 'ODD'),
+      tap(text => this.logger(text))
+    );
+    
+  }
+
+  logger(text: string): void {
+    console.log(`Log: ${text}`);
+  }
 }
