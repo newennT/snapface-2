83ad02dd4e4890eeea2d4cf3532a2fc6c886f775
NewennT <newenn.turbiau@gmail.com>
2026-01-09 16:29:43 +0100

    Validators

--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -77,4 +77,15 @@ export class FaceSnapsService {
     const faceSnap = this.getFaceSnapById(faceSnapId);
     snapType === 'snap' ? faceSnap.snaps++ : faceSnap.snaps--;
   }
+
+  addFaceSnap(formValue: { title: string; description: string; imageUrl: string; location: string }): void {
+    const faceSnap: FaceSnap = {
+      ...formValue,
+      createdDate: new Date(),
+      snaps: 0,
+      id: this.faceSnaps[this.faceSnaps.length - 1].id + 1
+    }
+    this.faceSnaps.push(faceSnap);
+  }
+  
 }
