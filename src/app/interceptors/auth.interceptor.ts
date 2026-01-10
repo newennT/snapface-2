7e6e71692bf00c78697121570d5ba4062ca60e62
NewennT <newenn.turbiau@gmail.com>
2026-01-10 20:23:21 +0100

    interceptors

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,17 @@
+import { HttpHeaders, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { HttpHandler, HttpEvent } from "@angular/common/http";
+import { AuthService } from "../services/auth.service";
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+    constructor(private authService: AuthService) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const headers = new HttpHeaders().append('Authorization', `Bearer ${this.authService.getToken()}`);
+        const modifiedReq = req.clone({ headers });
+        return next.handle(modifiedReq);
+        
+    }
+}
